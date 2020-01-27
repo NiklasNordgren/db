d788bbf7e69040a907990fef119020ecb4f32bdf
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-27 16:38:50 +0100

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE db.Person (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+CREATE TABLE db.Wtf (
+    WtfID int,
+);
