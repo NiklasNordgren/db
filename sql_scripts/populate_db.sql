d62a8818d69784c819d92bf38a5fd289fed0cfe1
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-30 16:11:33 +0100

    Revert "Revert "Update populate_db.sql""
    This reverts commit be0c56de7fba5693bec51afbdcc841cb3da6939d.

--- sql_scripts/populate_db.sql
+++ sql_scripts/populate_db.sql
@@ -1,5 +1,5 @@
 INSERT INTO db.User
-VALUES ('thu16nnn', 1);
+VALUES (0, 'thu16nnn', 1);
 
 INSERT INTO db.Academy
-VALUES ('Akademi för handel och avtal', 'AHA', 0);
+VALUES (0, 'Akademi för handel och avtal', 'AHA', 0);
